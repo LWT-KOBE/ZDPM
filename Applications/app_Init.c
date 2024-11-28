d013f1013b2dde043988ab60220684e801249a05
LWT-KOBE <1335980586@aliyun.com>
2024-11-28 11:07:13 +0800

    ZDPM V1

--- Applications/app_Init.c
+++ Applications/app_Init.c
@@ -7,23 +7,23 @@ void appInit(void *Parameters){
 	//NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	taskInitData.eventGroups = xEventGroupCreate();
 	/*- 在此处下写入初始化任务 -------*/
-	configInit();               			//加载默认参数
+	//configInit();               			//加载默认参数
 	//定时器5初始化,用于精确时间节点计算
 	clockClass.Init();	    
     //虚拟串口初始化
 	//usbVCP_Init(USB_USART_PreemptionPriority,USB_USART_SubPriority);        
 
     //监控机初始化
-	supervisorInit();	
+	//supervisorInit();	
 
 	OdriveInit();
 	//无线数传初始化
 	//wirelessInit();
 	//控制初始化
-	controlInit();
+	//controlInit();
 	
 	//小火车控制任务初始化
-	XHCDataInit();
+	//XHCDataInit();
 	
 	//CAN1控制任务初始化
 	CAN1DataInit();
